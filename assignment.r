4ebe9ca5a13aff158a00a6e3fe629041a9349f9a
liweichen <vivienclw@gmail.com>
2012-09-11 00:36:31 -0300

    Update assignment.r

--- assignment.r
+++ assignment.r
@@ -1,57 +1,57 @@
 1. What the sequences are representing and where they come from
-To understand how to design telehealth consultations to effectively virtualize the clinical consultation process, I am now coordinating with an Indian hospital in obtaining access to 400 video recordings of telehealth consultation sessions. I plan to code bodily-based interactions, verbal communication, and text communication among patients, physicians, and operators during each telehealth consultation session. The plausible coding categories will include (a) information content; (b) information representation, (c) information access, (d) the sequenceof activities, and (e) the outcomes in the telehealth consultation process.  
-
-This research project is still at an initial stage of conceptualization. Up till now, I have not got the data and I am not sure what we can do with the data yet. So the following answers to the assignment questions are one possible way to explore the data. I am looking forward to your feedback. I believe your suggestions will help me further develop interesting ideas and explore this phenomena in a deeper sense.
+   To understand how to design telehealth consultations to effectively virtualize the clinical consultation process, I am now coordinating with an Indian hospital in obtaining access to 400 video recordings of telehealth consultation sessions. I plan to code bodily-based interactions, verbal communication, and text communication among patients, physicians, and operators during each telehealth consultation session. The plausible coding categories will include (a) information content; (b) information representation, (c) information access, (d) the sequenceof activities, and (e) the outcomes in the telehealth consultation process.  
+   This research project is still at an initial stage of conceptualization. Up till now, I have not got the data and I am not sure what we can do with the data yet. So the following answers to the assignment questions are one possible way to explore the data. I am looking forward to your feedback. I believe your suggestions will help me further develop interesting ideas and explore this phenomena in a deeper sense.
 
 2. Whether there is one sequence per case or multichannel sequences? In case of multichannel sequences, specify to the following points for each channel
 •  My initial idea is to treat the data as one sequence per case, i.e., multiple activities/interactions within one teleconsultation. 
-•	Data will be coded for multiple stakeholders, including the activities performed by physicians, patients, and administrators, as well as the interactions between them. Thus it is possible to view the data as multichannel sequences.  I would love to learn more about multichannel sequences in our later sessions.
+•  Data will be coded for multiple stakeholders, including the activities performed by physicians, patients, and administrators, as well as the interactions between them. Thus it is possible to view the data as multichannel sequences.  I would love to learn more about multichannel sequences in our later sessions.
 
 3. The nature of the sequences (Categorical? Chronological? State or event sequences? )
-•	Categorical sequences.
-•	Event sequences. 
+•  Categorical sequences.
+•  Event sequences. 
 
 4. The alphabet (list of symbols in the sequences)
-The physician interacts with technologies
+   The physician interacts with technologies
 	-Use technologies
 	-Deal with technical problems
-The administer interacts with technologies:
-  -Use technologies
+   The administer interacts with technologies:
+ 	-Use technologies
 	-Deal with technical problems
-The patient interacts with technologies:
-  -Use technologies
+   The patient interacts with technologies:
+  	-Use technologies
 	-Deal with technical problems
-The physician interacts with patients: 
-  -Physician’s questions,
-  -Physician’s answers,
-  -Physician’s gestures/movements, 
-  -Physician’s expressions,
-  -Patient’s questions,
-  -Patient’s answers,
-  -Patient’s gestures/movements, 
-  -Patient’s expressions
-The physician interacts with administers
-  -Physician’s questions,
-  -Physician’s answers,
-  -Administer’s questions,
-  -Administer’s answers,
-The patient interacts with administers
-  -Patient’s questions,
-  -Patient’s answers,
-  -Administer’s questions,
-  -Administer’s answers,
+   The physician interacts with patients: 
+  	-Physician’s questions,
+  	-Physician’s answers,
+  	-Physician’s gestures/movements, 
+  	-Physician’s expressions,
+  	-Patient’s questions,
+  	-Patient’s answers,
+  	-Patient’s gestures/movements, 
+  	-Patient’s expressions
+   The physician interacts with administers
+  	-Physician’s questions,
+  	-Physician’s answers,
+  	-Administer’s questions,
+  	-Administer’s answers,
+   The patient interacts with administers
+  	-Patient’s questions,
+  	-Patient’s answers,
+  	-Administer’s questions,
+  	-Administer’s answers.
 
 5. Is there a natural order of the symbols (ordinal variable)?
-No.
+   No.
 
 6. Size of the alphabet
-22
+   22.
 
 7. Number of sequences
-•	Not specified yet
+•  Not specified yet.
 
 8. Maximum and minimum sequence lengths
-•	 Not specified yet
+•  Not specified yet.
+
 9. The kind of knowledge you expect to extract from your sequences What are you primarily interested in: sequencing, timing, duration, quantum?
-•	I am interested in most characteristics of the sequences, primarily sequencing, timing, and duration, within the telehealth consultation process.
-•	Primary aim is to assess how  technologies enhances the efficiency and effectiveness of the diagnosis process  and the communication process.  
+•  I am interested in most characteristics of the sequences, primarily sequencing, timing, and duration, within the telehealth consultation process.
+•  Primary aim is to assess how  technologies enhances the efficiency and effectiveness of the diagnosis process  and the communication process.  
